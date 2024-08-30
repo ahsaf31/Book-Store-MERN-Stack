1ce2c4eed1d3e1cf0ef6d826dde88f9b3563d3c3
ahsaf31 <ahsafahamed0@gmail.com>
2024-08-30 15:23:21 +0530

    Modified files in Ahsaf

--- backend/index.js
+++ backend/index.js
@@ -44,11 +44,69 @@ app.post('/books', async (request, response) => {
 app.get('/books',async(request,response)=>{
     try {
        const books =await Book.find({}); 
-       return response.status(200).json(books);
+       return response.status(200).json({
+        count:books.length,
+        data:books
+       });
 
     } catch (error) {
         console.log(error.message);
         response.status(500).send({message:error.message});
+    }
+});
+
+//Route for Get All Books from database by id
+app.get('/books/:id',async(request,response)=>{
+    try {
+
+        const {id}= request.params;
+       const book =await Book.findById(id); 
+       return response.status(200).json(book);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Route for Update a Book
+app.put('/books/:id',async (request,response) => {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields:title,author,publishYear',
+            });
+        }
+        const {id}= request.params;
+       const result =await Book.findByIdAndUpdate(id,request.body); 
+       if(!result){
+        return response.status(404).json({message: 'Book not found'});
+       }
+       return response.status(200).json({message: 'Book updated successfully'});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+        
+    }
+});
+
+//Route for Delete a Book
+app.delete('/books/:id',async (request,response) => {
+    try {
+        const {id}= request.params;
+       const result =await Book.findByIdAndDelete(id); 
+       if(!result){
+        return response.status(404).json({message: 'Book not found'});
+       }
+       return response.status(200).json({message: 'Book deleted successfully'});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+        
     }
 });
 
