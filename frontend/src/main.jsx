dd00a1f3fa733e7d48a8c55ddd76144850543bf7
ahsaf31 <ahsafahamed0@gmail.com>
2024-08-31 18:58:17 +0530

    Created and Modified in Ahsaf

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-)
+// Create the root element using createRoot
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
